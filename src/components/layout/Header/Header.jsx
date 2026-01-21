7ecf238c095945ac213f45193cf1f375a8495977
Daniela2319 <frankvt04@gmail.com>
2026-01-21 18:13:52 -0300

    feat(estrutura):estrutura inicial do projeto dashboard

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../../assets/logo.svg";
 
 export default function Header() {
   return (
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
       <main style={{ padding: "24px" }}>{children}</main>
+      <Footer />
     </>
   );
 }
